75fc2ec395dbcd4e27f11c5209c5bdfc59468709
Laqwanda Nettles <nettleslaqwanda@gmail.com>
2024-03-27 22:09:08 -0500

    created an input type object and export.

---
+++ questions/favoriteLanguage.js
@@ -0,0 +1,8 @@
+"use strict";
+const favoriteLanguage = {
+  type: "input",
+  name: "favoriteLanguage",
+  message: "What is your favorite programming language?",
+};
+
+export default favoriteLanguage;
