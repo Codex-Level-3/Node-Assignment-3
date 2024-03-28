0548c4b11859cb98c51f10b44feb0392cf7d5812
Laqwanda Nettles <nettleslaqwanda@gmail.com>
2024-03-27 22:09:38 -0500

    created an list type object and exported it

---
+++ questions/preferredFramework.js
@@ -0,0 +1,9 @@
+"use strict";
+const preferredFramework = {
+  type: "list",
+  name: "preferredFramework",
+  message: "What is your preferred framework?",
+  choices: ["React", "Angular JS", "Bootstrap", "Django"],
+};
+
+export default preferredFramework;
